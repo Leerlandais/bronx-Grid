07be55a0eecb1a797059e4b550029d3f694847c4
leerlandais <leerlandais@gmail.com>
2024-04-04 11:23:30 +0200

    home page OK and responsive

--- view/bronxHomeView.php
+++ view/bronxHomeView.php
@@ -15,8 +15,8 @@
 <?php
     require_once ("inc/headerView.php");
     require_once ("inc/navBarView.php");
- //   require_once ("inc/newsPartnerView.php");
- //   require_once ("inc/footerView.php");
+    require_once ("inc/newsPartnerView.php");
+    require_once ("inc/footerView.php");
 ?>
 
 
